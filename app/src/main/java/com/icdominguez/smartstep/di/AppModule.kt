08987bc86d64215469b09fd6c2da58dbf194dc5f
Ismael Cordón <56835908+icdominguez@users.noreply.github.com>
2026-02-09 15:27:35 +0100

    [001] Packages and libraries setup

---
+++ app/src/main/java/com/icdominguez/smartstep/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.icdominguez.smartstep.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
---
+++ app/src/main/java/com/icdominguez/smartstep/presentation/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.icdominguez.smartstep.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.icdominguez.smartstep.presentation.designsystem.SmartStepTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SmartStepTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Welcome to SmartStep 👟",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(name)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SmartStepTheme {
+        Greeting("Welcome to SmartStep 👟")
+    }
+}
