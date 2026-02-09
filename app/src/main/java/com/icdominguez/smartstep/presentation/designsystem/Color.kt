55b2f695449da8e31ff842251b7a354351b97646
Ismael Cordón <56835908+icdominguez@users.noreply.github.com>
2026-02-09 16:50:08 +0100

    [002] Setup theme, splash screen and app icon

--- app/src/main/java/com/icdominguez/smartstep/presentation/designsystem/Color.kt
+++ app/src/main/java/com/icdominguez/smartstep/presentation/designsystem/Color.kt
@@ -2,10 +2,14 @@ package com.icdominguez.smartstep.presentation.designsystem
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ButtonPrimary = Color(0xFF3A43B6)
+val ButtonSecondary = Color(0xFFE4E6FC)
+val TextPrimary = Color(0xFF1F2024)
+val TextSecondary = Color(0xFF6C6E71)
+val TextWhite = Color(0xFFFFFFFF)
+val BackgroundMain = Color(0xFFE4F0FB)
+val BackgroundSecondary = Color(0xFFF9FAFB)
+val BackgroundTertiary = Color(0xFFF0F0F0)
+val BackgroundWhite = Color(0xFFFFFFFF)
+val BackgroundWhite20 = Color(0x33FFFFFF)
+val StrokeMain = Color(0xFFE5E7EB)
