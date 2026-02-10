f15155701a285aa37da071166e5f08a8e710087f
icdominguez <56835908+icdominguez@users.noreply.github.com>
2026-02-10 10:53:15 +0100

    [004] Unit switcher

---
+++ app/src/main/java/com/icdominguez/smartstep/presentation/composables/switcher/UnitSwitcher.kt
@@ -0,0 +1,53 @@
+package com.icdominguez.smartstep.presentation.composables.switcher
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun UnitSwitcher(
+    modifier: Modifier = Modifier,
+    leftOption: String,
+    rightOption: String,
+    selectedOption: String = leftOption,
+    onOptionSelected: (String) -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        SwitcherOption(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            text = leftOption,
+            isSelected = selectedOption == leftOption,
+            onClick = { onOptionSelected(leftOption) },
+        )
+
+        SwitcherOption(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            text = rightOption,
+            isSelected = selectedOption == rightOption,
+            isRightOption = true,
+            onClick = { onOptionSelected(rightOption) },
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UnitSwitcherPreview() {
+    UnitSwitcher(
+        leftOption = "kg",
+        rightOption = "lbs",
+        onOptionSelected = {}
+    )
+}
